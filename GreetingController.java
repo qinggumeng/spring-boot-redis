4adf2c7646338d0a5bbd5f15ee831bba4fef2404
qinggumeng <353782686@qq.com>
2018-02-10 15:11:12 +0800

    1111

---
+++ GreetingController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+
+        redisUtils.set("123", "hello world");
+        System.out.println("进入了方法");
+        String string= redisUtils.get("123").toString();
+
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
